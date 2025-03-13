e20f5b9667c4285b8e918784ca962b14c82399b4
njongefred9 <frednjonge9@gmail.com>
2025-03-13 17:54:13 +0300

    Setup pundit gem

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ end
 
 gem "devise", "~> 4.9"
 gem "rolify", "~> 6.0"
+
+gem "pundit", "~> 2.5"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back_or_to(root_path)
+  end
 end
